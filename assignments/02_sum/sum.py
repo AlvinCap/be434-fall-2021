4d0279ef2ba981d47de47aeb303a74fe40e2bfdd
AlvinCap <capalvin@AO-Precision.localdomain>
2021-09-15 11:33:47 -0700

    newnewnew

--- assignments/02_sum/sum.py
+++ assignments/02_sum/sum.py
@@ -26,7 +26,7 @@ def get_args():
 def main():
     """Make a jazz noise here"""
     args = get_args()
-    
+    numList = args.nums
     print('{} = {}'.format('+'.join(map(str, numList)), sum(numList)))
 
 # --------------------------------------------------
