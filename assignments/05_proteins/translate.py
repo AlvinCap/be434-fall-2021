34fc5ff84dab5cb80c421d5b4799a740dae5b501
Alvin Onyango <aonyango@email.arizona.edu>
2021-10-06 07:31:54 -0700

    Proteinns

--- assignments/05_proteins/translate.py
+++ assignments/05_proteins/translate.py
@@ -2,10 +2,9 @@
 """
 Author : Alvin Onyango <capalvin@localhost>
 Date   : 2021-09-28
-Purpose: Counting Words
+Purpose: Protein Translate
 """
 import argparse
-import sys
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
@@ -13,39 +12,31 @@ def get_args():
         description='Translate',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('files',
-                        help='A readable input file(s)',
-                        metavar='File',
-                        nargs='*',
-                        default=[sys.stdin],
+    parser.add_argument('--c',
+                        '--codons',
+                        help='Codon Table for Translate',
+                        metavar='Codons', nargs='*',
                         type=argparse.FileType('rt'))
 
+    parser.add_argument('--o',
+                    '--output',
+                    help='Output File',
+                    type=argparse.FileType('wt'))
+                    
+    parser.add_argument('--s',
+                    '--sequence',
+                    help='The string sequence',
+                    metavar='str',
+                    type=str,
+                    default=None)
+                        
     return parser.parse_args()
-
 
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
     args = get_args()
-    total_lines = 0 
-    total_words = 0 
-    total_chars = 0
-
-    for fh in args.files:
-        num_lines = 0
-        num_words = 0
-        num_char = 0
-        for line in fh:
-            num_lines += 1
-            num_words += (len(line.split()))
-            num_char += len(line)
-        print('{:>8}{:>8}{:>8} {}'.format(num_lines, num_words, num_char, fh.name))
-        total_lines += num_lines 
-        total_words += num_words 
-        total_chars += num_char
-
-    if len(args.files) >1: 
-        print('{:>8}{:>8}{:>8} total'.format(total_lines, total_words, total_chars))
+    print(args)
 # -----------------------------------------------
 if __name__ == '__main__':
     main()
