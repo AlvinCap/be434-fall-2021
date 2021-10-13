2cbfcf5300b38b46783d76980f03f81f3e7317cd
Alvin Onyango <aonyango@email.arizona.edu>
2021-10-13 01:54:56 -0700

    Nw

--- assignments/06_common/common.py
+++ assignments/06_common/common.py
@@ -16,36 +16,19 @@ def get_args():
         description='Common Words',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
+    parser.add_argument('file1',
+                        help='Input File 1',
+                        metavar='FileOne',
+                        required=True, type=argparse.FileType('rt'))
+    parser.add_argument('file2',
+                        help='Input File 2',
+                        metavar='FileTwo',
+                        required=True, type=argparse.FileType('rt'))
     parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
-
+                        '--output',
+                        help='Output File',
+                        type=argparse.FileType('wt'), default='out.txt')
+                    
     return parser.parse_args()
 
 
@@ -54,17 +37,10 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
-
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    file1 = args.file1
+    file2 = args.file2
+    fileOut = args.output
+    
 
 
 # --------------------------------------------------
