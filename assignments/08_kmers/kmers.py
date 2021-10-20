bb7973b65fe9e13c7eab40ea73fd936565cc7737
Alvin Onyango <aonyango@email.arizona.edu>
2021-10-20 10:40:35 -0700

    push2

--- assignments/08_kmers/kmers.py
+++ assignments/08_kmers/kmers.py
@@ -8,6 +8,7 @@ Purpose: Rock the Casbah
 import argparse
 
 
+
 # --------------------------------------------------
 def get_args():
     """Get command-line arguments"""
@@ -16,35 +17,22 @@ def get_args():
         description='Rock the Casbah',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
+    parser.add_argument('FILE1',
+                        metavar='FILE1',
+                        help='Input file 1',
+                        type=argparse.FileType('rt'))
 
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
+    parser.add_argument('FILE2',
+                        metavar='FILE2',
+                        help='Input file 1',
+                        type=argparse.FileType('rt'))
 
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
+    parser.add_argument('-k',
+                        '--kmer',
+                        help='A named integer value greater than 0',
                         metavar='int',
                         type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
-
-    parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        default=3)
 
     return parser.parse_args()
 
@@ -54,17 +42,13 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
+    if args.kmer <= 0:
+        # return parser.error()
+    # else: 
+        # print(args.kmer)
 
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+
+
 
 
 # --------------------------------------------------
