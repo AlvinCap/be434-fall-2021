950959bf419bdbb0cd778c98d3febd5991458aa9
Alvin Onyango <aonyango@email.arizona.edu>
2021-12-09 22:08:14 -0700

    11_run

--- assignments/11_run_length/run.py
+++ assignments/11_run_length/run.py
@@ -17,27 +17,47 @@ def get_args():
         description='Rock the Casbah',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('inputval',
-                        help='A readable DNA text or file',
-                        metavar=str)
+    parser.add_argument('text',
+                        help='Input Text or File Handle',
+                        metavar='str')
 
-    return parser.parse_args()
+    args = parser.parse_args()
+
+    if os.path.isfile(args.text):
+        args.text = open(args.text).read().rstrip()
+    return args
 
 
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
     args = get_args()
-    inputv = args.inputval
-    if os.path.exists(inputv) and os.path.isfile(inputv):
-        input_type = 'f'
-        if input_type == 'f':
-            sequences = open(file=inputv, mode='rt', encoding='UTF-8')
-            seqfile = (sequences.readlines())
-            print(seqfile)
+    for seq in args.text.splitlines():
+        print(test_rle(seq))
+
+
+# --------------------------------------------------
+def test_rle(seq: str) -> str:
+    countsVal = []
+    count = 0
+    prev = ''
+    for char in seq:
+        if prev == '':
+            prev = char
+            count = 1
+        elif char == prev:
+            count += 1
         else:
-            seq = (inputv)
-            print(seq)
+            countsVal.append((prev, count))
+            count = 1
+            prev = char
+    countsVal.append((prev, count))
+
+    reg = ''
+    for char, count in countsVal:
+        reg += '{}{}'.format(char, count if count > 1 else '')
+
+    return reg
 
 
 # --------------------------------------------------
